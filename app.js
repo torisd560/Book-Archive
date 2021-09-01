daf13912faf6ff0d5a16fc1e94506aeabdbfc0a8
torisd560 <mitalisd123@gmail.com>
2021-09-02 02:34:44 +0600

    added cover image

--- app.js
+++ app.js
@@ -11,13 +11,14 @@ const searchBooks = () => {
 //display books on UI
 const displyBooks = books => {
     books.forEach(book => {
+        console.log(book)
         const bookContainer = document.getElementById('books-container');
         bookContainer.textContent - '';
         const div = document.createElement('div')
         div.classList.add('col')
         div.innerHTML = `
-            <div class="card">
-                <img src="" class="card-img-top" alt="...">
+            <div class="card h-100">
+                <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top img-fluid" alt="...">
                 <div class="card-body">
                         <h6 class="card-title"><strong>Book Name:</strong> ${book.title}</h6>
                         <p><strong>Author Name:</strong> ${book.author_name ? book.author_name : ''}<p>
