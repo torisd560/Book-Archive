81dbe0e60d1c965366335322ec717b93f16592ef
torisd560 <mitalisd123@gmail.com>
2021-09-02 02:14:51 +0600

    added display book site

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
 // search by bookname from API
-const searchBooks = () =>{
+const searchBooks = () => {
     const searchInput = document.getElementById('search-input');
     const searchText = searchInput.value;
-    searchInput.value='';
+    searchInput.value = '';
+    //load data from API
     fetch(`http://openlibrary.org/search.json?q=${searchText}`)
-    .then(res=>res.json())
-    .then(data=>console.log(data.docs))
+        .then(res => res.json())
+        .then(data => displyBooks(data.docs))
+}
+//display books on UI
+const displyBooks = books => {
+    books.forEach(book => {
+        const bookContainer = document.getElementById('books-container');
+        bookContainer.textContent - '';
+        const div = document.createElement('div')
+        div.classList.add('col')
+        div.innerHTML = `
+            <div class="card">
+                <img src="" class="card-img-top" alt="...">
+                <div class="card-body">
+                        <h6 class="card-title"><strong>Book Name:</strong> ${book.title}</h6>
+                        <p><strong>Author Name:</strong> ${book.author_name ? book.author_name : ''}<p>
+                        <p><strong>First publish year:</strong>  ${book.first_publish_year ? book.first_publish_year : ''}</p>
+                        <p><strong>Publisher: </strong> ${book.publisher}</p>
+                </div>
+            </div>
+            `;
+        bookContainer.appendChild(div)
+    });
 }
