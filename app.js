948c729821ca30ff902ce2f7f9c7367cccc90c9f
torisd560 <mitalisd123@gmail.com>
2021-09-02 11:44:40 +0600

    added errormessage

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+// error div
+const erreDiv =document.getElementById('error');
 // search by bookname from API
 const searchBooks = () => {
     const searchInput = document.getElementById('search-input');
     const searchText = searchInput.value;
+    if(searchText==''){
+        
+    }
     searchInput.value = '';
     //load data from API
     fetch(`http://openlibrary.org/search.json?q=${searchText}`)
@@ -10,20 +15,24 @@ const searchBooks = () => {
 }
 //display books on UI
 const displyBooks = books => {
+    // error message
+    if (books.length===0) {
+        erreDiv.style.display = 'block'
+    }
     books.forEach(book => {
-        console.log(book)
+        erreDiv.style.display = 'none'
         const bookContainer = document.getElementById('books-container');
         bookContainer.textContent - '';
         const div = document.createElement('div')
         div.classList.add('col')
         div.innerHTML = `
             <div class="card h-100">
-                <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top img-fluid" alt="...">
+                <img src="https://covers.openlibrary.org/b/id/${book.cover_i ? book.cover_i : ''}-M.jpg" class="card-img-top img-fluid h-75" alt="...">
                 <div class="card-body">
                         <h6 class="card-title"><strong>Book Name:</strong> ${book.title}</h6>
                         <p><strong>Author Name:</strong> ${book.author_name ? book.author_name : ''}<p>
                         <p><strong>First publish year:</strong>  ${book.first_publish_year ? book.first_publish_year : ''}</p>
-                        <p><strong>Publisher: </strong> ${book.publisher}</p>
+                        <p><strong>Publisher: </strong> ${book.publisher[0]}</p>
                 </div>
             </div>
             `;
