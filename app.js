eb1b174c686d4c5b9d280b2cc9fcfb42d33b00a2
torisd560 <mitalisd123@gmail.com>
2021-09-02 16:02:41 +0600

    added spinner

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
+//----------------spinner------------------------------------
+const toggleSpinner= displayStyle => {
+    document.getElementById('spinner').style.display = displayStyle;
+}
 //-------------------- found result----------------------------
 const searchResultContainer = document.getElementById('search-result');
 const foundResult = document.getElementById('found-result')
 //----------------error div----------------------------
 const erreDiv = document.getElementById('error');
-//---------------search by bookname from API---------------------
+//---------------search feild---------------------
 const searchBooks = () => {
     const searchInput = document.getElementById('search-input');
     const searchText = searchInput.value;
+    //----------display spinner---------------------
+    toggleSpinner('block')
     //-----------------clear search input--------------------
     searchInput.value = '';
     //------------load data from API----------------
@@ -14,7 +20,7 @@ const searchBooks = () => {
         .then(res => res.json())
         .then(data => displyBooks(data.docs))
 }
-//----------------------------display books on UI-----------------------------------
+//----------------------------display books on UI-----------------------
 const displyBooks = books => {
     //---------------error message---------------------------------------
     if (books.length === 0) {
@@ -39,10 +45,11 @@ const displyBooks = books => {
                         <h6 class="card-title"><strong>Book Name:</strong> ${book.title}</h6>
                         <p><strong>Author Name:</strong> ${book.author_name ? book.author_name : ''}<p>
                         <p><strong>First publish year:</strong>  ${book.first_publish_year ? book.first_publish_year : ''}</p>
-                        <p><strong>Publisher: </strong> ${book.publisher[0]}</p>
+                        <p><strong>Publisher: </strong> ${book.publisher ?book.publisher:''}</p>
                 </div>
             </div>
             `;
-        bookContainer.appendChild(div)
+        bookContainer.appendChild(div);
+        toggleSpinner('none')
     });
 }
