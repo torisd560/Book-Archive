40439dc71905814a8c69c8199382daff78428b79
torisd560 <mitalisd123@gmail.com>
2021-09-02 13:46:56 +0600

    added updates

--- app.js
+++ app.js
@@ -1,37 +1,33 @@
+//-------------------- found result----------------------------
 const searchResultContainer = document.getElementById('search-result');
 const foundResult = document.getElementById('found-result')
-
-
-
-
-
-// error div
-const erreDiv =document.getElementById('error');
-// search by bookname from API
+//----------------error div----------------------------
+const erreDiv = document.getElementById('error');
+//---------------search by bookname from API---------------------
 const searchBooks = () => {
     const searchInput = document.getElementById('search-input');
     const searchText = searchInput.value;
-    if(searchText==''){
-        
-    }
+    //-----------------clear search input--------------------
     searchInput.value = '';
-    //load data from API
-    fetch(`http://openlibrary.org/search.json?q=${searchText}`)
+    //------------load data from API----------------
+    fetch(`https://openlibrary.org/search.json?q=${searchText}`)
         .then(res => res.json())
         .then(data => displyBooks(data.docs))
 }
-//display books on UI
+//----------------------------display books on UI-----------------------------------
 const displyBooks = books => {
-    // error message
-    if (books.length===0) {
+    //---------------error message---------------------------------------
+    if (books.length === 0) {
         erreDiv.style.display = 'block'
     }
+    //------------------clear display data-------------------------
+    const bookContainer = document.getElementById('books-container');
+    bookContainer.textContent = '';
+    //----------------------forEatch get book----------------------
     books.forEach(book => {
         searchResultContainer.style.display = 'block';
         foundResult.innerText = books.length;
         erreDiv.style.display = 'none'
-        const bookContainer = document.getElementById('books-container');
-        bookContainer.textContent - '';
         const div = document.createElement('div')
         div.classList.add('col')
         div.innerHTML = `
